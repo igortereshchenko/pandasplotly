61a97268dbfdd54d8b637c31375378d8609ea361
aAndrew <mirik23452@gmail.com>
2019-12-16 18:41:57 +0200

    pie

--- miroshnykAV/main.py
+++ miroshnykAV/main.py
@@ -1,19 +1,3 @@
-# import os
-# import pandas as pd
-# from bq_helper import BigQueryHelper
-# import plotly.graph_objs as go
-# from plotly.offline import plot
-# os.environ["GOOGLE_APPLICATION_CREDENRIALS"]="keys.json"
-# from google.cloud import bigquery
-# client =bigquery.Client()
-# data_ref=client.dataset('san_francisco',project='bigquery-public-data')
-# bg_asisten=BigQueryHelper('bigquery-public-data','san_francisco')
-# QUERY = """
-#         SELECT  FROM
-#         LIMIT 10000
-#         """
-# df=bg_asisten.query_
-# print(type(data_ref))
 
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="keys.json"
@@ -27,25 +11,22 @@ bq_assistant = BigQueryHelper('bigquery-public-data','san_francisco')
 
 
 QUERY = """
-        SELECT unique_key, status,agency_name,created_date FROM `bigquery-public-data.san_francisco.street_trees`
-        LIMIT 100
+        SELECT species, site_order, plant_date ,tree_id
+        FROM `bigquery-public-data.san_francisco.street_trees`
+        LIMIT 150
         """
 
 
 df = bq_assistant.query_to_pandas(QUERY)
 
 
+bar=go.Bar(x=df['species'],y=df["site_order"])
+layout=go.Layout(title='species',xaxis=dict(title="species"),yaxis=dict(title="site_order"),)
+fig=go.Figure(data=[bar],layout=layout)
+plot(fig)
 
+labels = df['species']
+values = df["site_order"]
 
-female_df = df[df['status']=='closed'];
-
-female_df_groupby = female_df.groupby(['agency_name'])[].sum()
-
-
-trace1 = go.Scatter(
-                    x = female_df_groupby.index,
-                    y = female_df_groupby.values,
-                    mode = "closed",
-                    name = "agency",
-                    )
-print(df.head(10))
+fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+plot(fig)
